e2c54da8f540eeaa9fcf83b0ef1099b2e664d538
birdfrozen <530517399@qq.com>
2022-12-18 15:29:42 +0800

    changes

--- MyModel.py
+++ MyModel.py
@@ -45,20 +45,112 @@ class HSI_CLS_PCA_model(nn.Module):
         return out
 
 
-# class HybridSN()
-#    def __init__(self, band_num, pretrain=False):
-#         self.f0 = nn.Sequential(
-#             nn.Conv2d(band_num, 3, kernel_size=7, stride=1, padding=3),
-#             nn.BatchNorm2d(3),
-#             nn.ReLU(True),
-#         )
+class HybridSN(nn.Module):
+    def __init__(self, band_num, pretrain=False):
+        super().__init__()
+        # self.layer3d1 = nn.Conv3d(1, 8, kernel_size=(3,3,3), stride=1, padding=1)
+        self.layer3d = nn.Sequential(
+            nn.Conv3d(1, 8, kernel_size=(3,3,3), stride=1, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(True),
+            nn.Conv3d(8, 16, kernel_size=(3,3,3), stride=1, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(True),
+            nn.Conv3d(16, 32, kernel_size=(3,3,3), stride=1, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(True),
+        )
+        self.layer2d = nn.Sequential(
+            nn.Conv2d(band_num*32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((10,10))
+        self.layer1d = nn.Sequential(
+            nn.Linear(6400, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(True),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(True),
+            nn.Linear(128, 1),
+        )
 
-#     def forward(self, input):
+    def forward(self, input):
+        out = self.layer3d(input)
+        # out = self.layer3d1(input)
+        # out = self.layer3d(out)
+        conv3d_shape = out.shape
+        out = out.reshape([conv3d_shape[0],conv3d_shape[1]*conv3d_shape[2],conv3d_shape[3],conv3d_shape[4]])
+        out = self.layer2d(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.layer1d(out)
+        out = torch.sigmoid(out)
+        return out
 
+class multiclass_HybridSN(nn.Module):
+    def __init__(self, band_num, cls_num, pretrain=False):
+        super().__init__()
+        # self.layer3d1 = nn.Conv3d(1, 8, kernel_size=(3,3,3), stride=1, padding=1)
+        self.layer3d = nn.Sequential(
+            nn.Conv3d(1, 8, kernel_size=(3,3,3), stride=1, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(True),
+            nn.Conv3d(8, 16, kernel_size=(3,3,3), stride=1, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(True),
+            nn.Conv3d(16, 32, kernel_size=(3,3,3), stride=1, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(True),
+        )
+        self.layer2d = nn.Sequential(
+            nn.Conv2d(band_num*32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((10,10))
+        self.layer1d = nn.Sequential(
+            nn.Linear(6400, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(True),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(True),
+            nn.Linear(128, cls_num),
+        )
+
+    def forward(self, input):
+        out = self.layer3d(input)
+        # out = self.layer3d1(input)
+        # out = self.layer3d(out)
+        conv3d_shape = out.shape
+        out = out.reshape([conv3d_shape[0],conv3d_shape[1]*conv3d_shape[2],conv3d_shape[3],conv3d_shape[4]])
+        out = self.layer2d(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.layer1d(out)
+        out = torch.nn.functional.softmax(out,dim=1)
+        return out
 
 if __name__ == '__main__':
-    model = HSI_CLS_model(603)
-    input = torch.rand([3, 603, 200, 200])
+    # model = HSI_CLS_model(603)
+    # input = torch.rand([3, 603, 200, 200])
+    # print(type(input))
+    # output = model(input)
+    # print(output.size())
+
+    # model = HybridSN(10)
+    # input = torch.rand([3, 1, 10, 200, 200])
+    # # input = torch.rand([3, 10, 200, 200])
+    # print(type(input))
+    # output = model(input)
+    # print(output.size())
+
+    model = multiclass_HybridSN(10,9)
+    input = torch.rand([3, 1, 10, 200, 200])
+    # input = torch.rand([3, 10, 200, 200])
     print(type(input))
     output = model(input)
+    print(output)
     print(output.size())
--- estimation.py
+++ estimation.py
@@ -39,3 +39,33 @@ def hsic(Kx, Ky):
     n = Kxy.shape[0]
     h = np.trace(Kxy) / n**2 + np.mean(Kx) * np.mean(Ky) - 2*np.mean(Kxy)/n
     return h * n ** 2 / (n - 1)**2
+
+#计算准确率
+def calculate_all_prediction(confMatrix):
+    '''
+    计算总精度,对角线上所有值除以总数
+    :return:
+    '''
+    total_sum=confMatrix.sum()
+    correct_sum=(np.diag(confMatrix)).sum()
+    prediction=round(100*float(correct_sum)/float(total_sum),2)
+    print('准确率:'+str(prediction)+'%')
+
+def calculae_lable_prediction(confMatrix):
+    '''
+    计算每一个类别的预测精度:该类被预测正确的数除以该类的总数
+    '''
+    l=len(confMatrix)
+    for i in range(l):
+        label_total_sum = confMatrix.sum(axis=1)[i]
+        label_correct_sum=confMatrix[i][i]
+        prediction = round(100 * float(label_correct_sum) / float(label_total_sum), 2)
+        print('精确率:'+classes[i]+":"+str(prediction)+'%')
+
+def calculate_label_recall(confMatrix):
+    l = len(confMatrix)
+    for i in range(l):
+        label_total_sum = confMatrix.sum(axis=0)[i]
+        label_correct_sum = confMatrix[i][i]
+        prediction = round(100 * float(label_correct_sum) / float(label_total_sum), 2)
+        print('召回率:'+classes[i] + ":" + str(prediction) + '%')
